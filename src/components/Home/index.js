0b012d621e9486e78cf5bc75ca33032cb007e3ca
Valeriy Khupenia <thecokser@gmail.com>
2019-11-25 18:57:20 +0300

    added eslint config

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import withLoading from "../../shared/hoc/withLoading/withLoading";
 
 const HomeComponent = ({...props}) => {
-    console.log(props.data);
     return <h1>Page Founded</h1>;
 };
 
